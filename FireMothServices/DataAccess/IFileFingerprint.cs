1e667eda761e6c6720b984285bb2c15004e828bf
Jason Sells <jasonsells@protonmail.com>
2020-10-27 07:57:49 -0500

    Add files missed in previous commit

---
+++ FireMothServices/DataAccess/IFileFingerprint.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="IFileFingerprint.cs" company="Dark Hours Development">
+// Copyright (c) Dark Hours Development. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace FireMothServices.DataAccess
+{
+    public interface IFileFingerprint
+    {
+        string DirectoryName { get; set; }
+
+        string Name { get; set; }
+
+        long Length { get; set; }
+
+        string Base64Hash { get; set; }
+    }
+}
