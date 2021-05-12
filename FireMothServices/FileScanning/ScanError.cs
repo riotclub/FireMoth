126751d067757ff81af60b6ebdbbb879ee4f9c00
Jason Sells <jasonsells@protonmail.com>
2021-05-12 18:17:30 -0500

    Add documentation to ScanError

--- FireMothServices/FileScanning/ScanError.cs
+++ FireMothServices/FileScanning/ScanError.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="ScanError.cs" company="Dark Hours Development">
+// Copyright (c) Dark Hours Development. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
 
 namespace RiotClub.FireMoth.Services.FileScanning
 {
+    using System;
+
+    /// <summary>
+    /// Represents an error that occured during a file scan.
+    /// </summary>
     public class ScanError
     {
-        public string Path { get; set; }
+        /// <summary>
+        /// Gets or sets the path to the file or directory related to this error.
+        /// </summary>
+        public string? Path { get; set; }
 
+        /// <summary>
+        /// Gets or sets any exception related to this error.
+        /// </summary>
         public Exception? Exception { get; set; }
-
-
     }
 }
