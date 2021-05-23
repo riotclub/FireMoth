d6b36f24dcbcc02615c14967dabcc640fbe664d3
Jason Sells <jasonsells@protonmail.com>
2021-05-23 14:33:28 -0500

    Add index values to FileFingerprintMap

--- FireMothServices/DataAccess/FileFingerprintMap.cs
+++ FireMothServices/DataAccess/FileFingerprintMap.cs
@@ -1,11 +1,6 @@
 ﻿namespace RiotClub.FireMoth.Services.DataAccess
 {
     using CsvHelper.Configuration;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     /// <summary>
     /// A mapping between an <see cref="IFileInfo"/> and CSV headers.
@@ -18,10 +13,18 @@
         /// </summary>
         public FileFingerprintMap()
         {
-            this.Map(fingerprint => fingerprint.FileInfo.DirectoryName).Name("DirectoryName");
-            this.Map(fingerprint => fingerprint.FileInfo.Name).Name("Name");
-            this.Map(fingerprint => fingerprint.FileInfo.Length).Name("Length");
-            this.Map(fingerprint => fingerprint.Base64Hash).Name("Base64Hash");
+            this.Map(fingerprint => fingerprint.FileInfo.DirectoryName)
+                .Name("DirectoryName")
+                .Index(0);
+            this.Map(fingerprint => fingerprint.FileInfo.Name)
+                .Name("Name")
+                .Index(1);
+            this.Map(fingerprint => fingerprint.FileInfo.Length)
+                .Name("Length")
+                .Index(2);
+            this.Map(fingerprint => fingerprint.Base64Hash)
+                .Name("Base64Hash")
+                .Index(3);
         }
     }
 }
