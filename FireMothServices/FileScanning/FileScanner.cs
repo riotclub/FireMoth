e832f674ad347fc6f70431d5d7b42fd34a7e4818
Jason Sells <jasonsells@protonmail.com>
2021-05-23 14:34:14 -0500

    Fix error handling in FileScanner

--- FireMothServices/FileScanning/FileScanner.cs
+++ FireMothServices/FileScanning/FileScanner.cs
@@ -121,6 +121,9 @@ namespace RiotClub.FireMoth.Services.FileScanning
                     scanResult.SkippedFiles.Add(
                         file.FullName,
                         string.Format(messageTemplate, file.FullName, exception.Message));
+                    scanResult.Errors.Add(
+                        new ScanError(
+                            file.FullName, "Could not add record for file.", exception));
                 }
             }
         }
