bf351738ef4eb9bd7e02a15a3f7e99e33f6a901b
Jason Sells <jasonsells@protonmail.com>
2023-08-09 11:49:16 -0500

    Update FileFingerprint entity type configuration

--- FireMothServices/DataAccess/Sqlite/FileFingerprintTypeConfiguration.cs
+++ FireMothServices/DataAccess/Sqlite/FileFingerprintTypeConfiguration.cs
@@ -8,38 +8,18 @@ namespace RiotClub.FireMoth.Services.DataAccess.EntityFrameworkSqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using RiotClub.FireMoth.Services.Repository;
-using System.IO.Abstractions;
 
 internal class FileFingerprintTypeConfiguration : IEntityTypeConfiguration<FileFingerprint>
 {
     /// <inheritdoc/>
     public void Configure(EntityTypeBuilder<FileFingerprint> builder)
     {
-        builder.HasKey(f => new { f.DirectoryName, f.FileName, f.Base64Hash });
-
-        //builder.Property(f => f.FileInfo.DirectoryName)
-        //       .HasColumnName("Directory");
-
-        //builder.Property(f => f.FileInfo.Name)
-        //       .HasColumnName("FileName");
-
-        //builder.Property(f => f.FileInfo.Length)
-        //       .HasColumnName("FileSize");
-
-        //builder.Property(f => f.Base64Hash)
-        //       .HasColumnName("Base64Hash");
-
-        builder.Property("DirectoryName")
-            .HasColumnType("varchar(500)")
-            .HasColumnName("DirectoryName");
-
         builder.Property("FileName")
-            .HasColumnName("FileName");
-
-        builder.Property("FileSize")
-            .HasColumnName("FileSize");
-
+               .HasColumnType("varchar(256)");
+        builder.Property("DirectoryName")
+               .HasColumnType("varchar(1000)");
+        builder.Property("FileSize");
         builder.Property("Base64Hash")
-            .HasColumnName("Base64Hash");
+               .HasColumnType("char(44)");
     }
 }
